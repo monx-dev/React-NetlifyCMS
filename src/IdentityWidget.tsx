fe4e57cda5328501e968d0a40a94dac3b09007c6
Rafael Santos <rafaelnsantos1@gmail.com>
2020-10-25 21:33:30 -0300

    fix: identity onLoad

--- src/IdentityWidget.tsx
+++ src/IdentityWidget.tsx
@@ -3,11 +3,11 @@ import identity, { InitOptions } from 'netlify-identity-widget';
 
 export type OnLoad<T> = (item: T) => void | (() => void | undefined);
 
-export type Identity = typeof identity;
+const Identity = { ...identity };
 
 export interface IdentityWidgetProps {
   config: InitOptions;
-  onLoad?: OnLoad<Identity>;
+  onLoad?: OnLoad<typeof Identity>;
 }
 
 export const IdentityWidget = (props: IdentityWidgetProps) => {
