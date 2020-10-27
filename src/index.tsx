80a49e4c2b72a6ee55bc568941645bcff5d6a1ab
Rafael Santos <rafaelnsantos1@gmail.com>
2020-10-27 12:01:38 -0300

    refactor: move types to dist folder

--- src/index.tsx
+++ src/index.tsx
@@ -5,5 +5,5 @@ export { NetlifyCMS } from './NetlifyCMS';
 export type { CmsConfigFixed as CmsConfig, NetlifyCMSProps } from './NetlifyCMS';
 export type { IdentityWidgetProps } from './IdentityWidget';
 
-export type { CmsCollection } from 'netlify-cms-core';
+export type { CmsCollection, CmsField } from 'netlify-cms-core';
 export type { InitOptions as IdentityConfig } from 'netlify-identity-widget';
