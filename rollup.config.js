a57677d8d744e47934992b51e94ef7b7e9c5c4cf
Rafael Santos <rafaelnsantos1@gmail.com>
2020-10-27 12:02:23 -0300

    feat: add Widget constructor

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@ import { DEFAULT_EXTENSIONS } from '@babel/core';
 import { dependencies } from './package.json';
 
 export default {
-  input: ['src/index.tsx'],
+  input: ['src/index.tsx', 'src/Widget.tsx'],
   output: [
     {
       dir: 'dist',
