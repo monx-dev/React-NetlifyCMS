a091ef8c0158fb30abf445a55180b504f1fa7f97
Rafael Santos <rafaelnsantos1@gmail.com>
2020-11-06 13:10:55 -0300

    fix: build

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,7 @@ import configRollbar from '@monx/rollup-config';
 import { dependencies, peerDependencies } from './package.json';
 
 export default configRollbar({
-  input: ['src/index.ts'],
+  input: ['src/index.ts', 'src/Preview.tsx', 'src/Widget.tsx'],
   dependencies,
   peerDependencies,
-  format: 'esm',
 });
