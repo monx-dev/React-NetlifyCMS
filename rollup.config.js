ab2853c96ad13a511c83a0dc3b9bdc47d7794128
Rafael Santos <rafaelnsantos1@gmail.com>
2020-11-06 14:56:52 -0300

    feat: build with umd format

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,8 @@ import configRollbar from '@monx/rollup-config';
 import { dependencies, peerDependencies } from './package.json';
 
 export default configRollbar({
-  input: ['src/index.ts', 'src/Preview.tsx', 'src/Widget.tsx'],
+  input: 'src/index.ts',
   dependencies,
   peerDependencies,
+  format: 'umd',
 });
